e0b40fc688fda6d76f516d154451a9374db350b1
Brown Nguyễn <brownnguyen@gmail.com>
2020-10-13 16:52:58 +0700

    add__disabled scroll

--- js/general.js
+++ js/general.js
@@ -1,15 +1,7 @@
 var headerMenu = document.querySelector('.header-menu');
 var drawer = document.querySelector('.drawer-menu');
 var header = document.querySelector('.header');
-headerMenu.onclick = function () {
-    headerMenu.classList.toggle('active');
-    if (headerMenu.classList.contains('active')) {
-        drawer.classList.add('active')
-    }
-    else {
-        drawer.classList.remove('active')
-    }
-}
+
 window.onscroll = function () {
     var height = window.pageYOffset;
     if (height > 100) {
@@ -19,3 +11,60 @@ window.onscroll = function () {
         header.classList.remove('active');
     }
 }
+
+$(function () {
+    headerMenu.onclick = function () {
+        headerMenu.classList.toggle('active');
+        if (headerMenu.classList.contains('active')) {
+            drawer.classList.add('active');
+            Scroll.disable();
+        }
+        else {
+            drawer.classList.remove('active');
+            Scroll.enable();    
+        }
+    }
+    const Scroll = (function () {
+        // left: 37, up: 38, right: 39, down: 40,
+        // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+        var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+        function preventDefault(e) {
+            e.preventDefault();
+        }
+        function preventDefaultForScrollKeys(e) {
+            if (keys[e.keyCode]) {
+                preventDefault(e);
+                return false;
+            }
+        }
+        var supportsPassive = false;
+        try {
+            document.body.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+                get: function () { supportsPassive = true; }
+            }));
+        } catch (e) {
+            console.log('not catch error');
+        }
+        var wheelOpt = supportsPassive ? { passive: false } : false;
+        var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+        // call this to Disable
+        function disableScroll() {
+            document.body.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+            document.body.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+            document.body.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+            document.body.addEventListener('keydown', preventDefaultForScrollKeys, false);
+        }
+
+        // call this to Enable
+        function enableScroll() {
+            document.body.removeEventListener('DOMMouseScroll', preventDefault, false);
+            document.body.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+            document.body.removeEventListener('touchmove', preventDefault, wheelOpt);
+            document.body.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+        }
+        return {
+            disable: disableScroll,
+            enable: enableScroll
+        }
+    })();
+});
